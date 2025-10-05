b5ed076aa292e45c0f1f95a7bac879f1447f80dd
AABGRR <grupo6.prit@gmail.com>
2025-10-05 18:30:51 -0300

    menu

--- src/pages/Header/header.jsx
+++ src/pages/Header/header.jsx
@@ -1,19 +1,35 @@
-import { Link } from 'react-router-dom';
-import './header.css';
+import { Link } from "react-router-dom";
+import { useState } from "react";
+import "./header.css";
 
 function Header() {
+  const [menuOpen, setMenuOpen] = useState(false);
+
   return (
     <header className="menu-header">
       <div className="menu-container">
-        <h1 className="menu-logo">LEARNLY</h1>
+        {/* Botão hamburguer no lado esquerdo */}
+        <button
+          className={`menu-toggle ${menuOpen ? "open" : ""}`}
+          onClick={() => setMenuOpen(!menuOpen)}
+        >
+          <span></span>
+          <span></span>
+          <span></span>
+        </button>
 
-        <nav className="menu-nav">
-          <Link to="/home" className="menu-link">Home</Link>
-          <Link to="/produto" className="menu-link">Produto</Link>
-          <Link to="/generico" className="menu-link">Curso</Link>
-          <Link to="/perfil" className="menu-link">Perfil</Link>
-        </nav>
+        {/* Logo centralizado */}
+        <h1 className="menu-logo">LEARNLY</h1>
+        
       </div>
+
+      {/* Menu lateral */}
+      <nav className={`menu-nav ${menuOpen ? "active" : ""}`}>
+        <Link to="/home" className="menu-link" onClick={() => setMenuOpen(false)}>Home</Link>
+        <Link to="/produto" className="menu-link" onClick={() => setMenuOpen(false)}>Produto</Link>
+        <Link to="/generico" className="menu-link" onClick={() => setMenuOpen(false)}>Curso</Link>
+        <Link to="/perfil" className="menu-link" onClick={() => setMenuOpen(false)}>Perfil</Link>
+      </nav>
     </header>
   );
 }
